31b6505a1e083c0dd6f06a8d9edbb72c908d6f85
Matt Newton <newton10471@gmail.com>
2013-11-11 18:36:14 -0500

    initial environment config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huddle::Application.config.secret_token = '56b8fc9bb6a2f84af546428415bddc165c8a5bb67d217f810f61b1794561367fc1a32289dafed95dfc22f2a426883af98c510f406657636cd63350a5d3cc972c'
