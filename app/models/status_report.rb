1d05a1998bdebf4457b8a006195503b501e1db4c
Matt Newton <newton10471@gmail.com>
2013-11-12 06:34:33 -0500

    up to section 3.3

--- app/models/status_report.rb
+++ app/models/status_report.rb
@@ -1,6 +1,11 @@
 class StatusReport < ActiveRecord::Base
-	attr_accessible :project_id, :user_id
-  belongs_to :project
+ 	attr_accessible :status_date, :today, :yesterday, :project_id, :user_id
+ 	belongs_to :project
   belongs_to :user
-  attr_accessible :status_date, :today, :yesterday
+
+	before_save :set_status_date
+
+	def set_status_date
+		self.status_date = Date.today if status_date.nil?
+	end
 end
--- test/unit/status_report_test.rb
+++ test/unit/status_report_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class StatusReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "saving a status report saves the status date" do
+  	actual = StatusReport.new
+  	actual.save
+  	assert_equal(Date.today.to_s, actual.status_date.to_s)
+  end
+
+  test "saving a status report that has a date doesn't override" do
+  	actual = StatusReport.new
+  	actual.save
+  	actual.reload
+  	assert_equal(10.days.ago.to_date.to_s, actual.status_date.to_s)
+  end
 end
