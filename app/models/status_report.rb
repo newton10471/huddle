4aace984f83d264bca32ff5f6480803b1407de0f
Matt Newton <newton10471@gmail.com>
2013-11-12 07:22:55 -0500

    completed section 3.3

--- app/models/status_report.rb
+++ app/models/status_report.rb
@@ -4,8 +4,15 @@ class StatusReport < ActiveRecord::Base
   belongs_to :user
 
 	before_save :set_status_date
+	validate :validate_has_at_least_one_status
 
 	def set_status_date
 		self.status_date = Date.today if status_date.nil?
+	end
+
+	def validate_has_at_least_one_status
+		if today.blank? && yesterday.blank?
+			errors[:base] << "Must have at least one status set"
+		end
 	end
 end
